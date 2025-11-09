0a792b9712b5f122e1d2c0f39e7b9544a9694ed0
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:15 -0800

    tests

--- basic_test.go
+++ basic_test.go
@@ -1,17 +1,16 @@
 package split_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/split"
 )
 
 func ExampleSplit_basic() {
 	// split -l 10 input.txt output_prefix
 	// Note: This writes to files
-	yup.MustRun(
+	gloo.MustRun(
 		Split("testdata/input.txt", "output_prefix", Lines(10)),
 	)
 	// Output:
 	//
 }
-
